71fb9d4f70d9a303d6842aa5c50a2bf8a975ff03
sadzerolol <dr.kurilets23@gmail.com>
2024-07-21 14:44:43 +0300

    Async implemented

---
+++ Async/AsincFileParser/CreateRandomTextFile.cs
@@ -0,0 +1,38 @@
+﻿namespace AsincFileParser
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    public static class CreateRandomTextFile
+    {
+        public static async Task CreateRandomTextFileAsync(string filePath, SemaphoreSlim semaphore)
+        {
+            await semaphore.WaitAsync();
+            try
+            {
+                await Task.Delay(3000);
+                string content = GenerateRandomText(100);
+                await File.WriteAllTextAsync(filePath, content);
+                Console.WriteLine($"File created: {filePath}");
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+
+        public static string GenerateRandomText(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            Random random = new Random();
+            StringBuilder result = new StringBuilder(length);
+            for (int i = 0; i < length; i++)
+            {
+                result.Append(chars[random.Next(chars.Length)]);
+            }
+            return result.ToString();
+        }
+    }
+}
--- Async/AsincFileParser/Program.cs
+++ Async/AsincFileParser/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AsincFileParser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+int numberOfFiles = 10;
+int maxConcurrency = 3;
+string directoryPath = "RandomTextFiles";
+Directory.CreateDirectory(directoryPath);
+
+List<string> filePaths = new List<string>();
+
+for (int i = 0; i < numberOfFiles; i++)
+{
+    filePaths.Add(Path.Combine(directoryPath, $"file{i + 1}.txt"));
+}
+
+SemaphoreSlim semaphore = new SemaphoreSlim(maxConcurrency);
+List<Task> tasks = new List<Task>();
+
+foreach (var filePath in filePaths)
+{
+    tasks.Add(CreateRandomTextFile.CreateRandomTextFileAsync(filePath, semaphore));
+}
+
+await Task.WhenAll(tasks);
+Console.WriteLine("All files created.");
